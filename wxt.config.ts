0f9d685167d7d1c8346b409eb03dd48553a4dc77
nuttycc <73019368+nuttycc@users.noreply.github.com>
2025-12-09 16:56:22 +0800

    chore/project-metadata-update (#23)
    * chore: update project metadata and dependencies
    
    * chore: add new extension icon
    
    * docs: update project description
    
    * docs: add store screenshot and adjust image widths
    
    * docs: add Chrome Web Store link to README

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,12 +4,13 @@ import { defineConfig } from "wxt";
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   srcDir: "./src",
-  modules: ["@wxt-dev/module-vue"],
+  modules: ["@wxt-dev/module-vue", "@wxt-dev/auto-icons"],
   webExt: {
     disabled: true,
   },
   manifest: {
     name: "Lumin Time",
+    homepage_url: "https://github.com/nuttycc/LuminTime",
     permissions: ["tabs", "storage", "webNavigation", "idle", "alarms"],
   },
   vite: () => ({
